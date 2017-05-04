fa350552a6f682a1250941702cf27f3ecb1ca00c
Patrick Hussey <husspm@gmail.com>
2017-05-04 15:44:05 -0400

    pushing changes to connections page

--- app/config/connection.js
+++ app/config/connection.js
@@ -1,5 +1,9 @@
 //DBA = DataBaseAccess
 var DBA = require("mysql"),
+    connection;
+if (process.env.JAWSDB_URL) {
+    connection = DBA.createConnection(process.env.JAWSDB_URL);
+} else {
     connection = DBA.createConnection({
         host: "localhost",
         port: 3306,
@@ -7,6 +11,7 @@ var DBA = require("mysql"),
         password: "qaz123wsx456",
         database: "burgers_db"
     });
+}
 connection.connect(function(err) {
     if (err) throw err;
     //again for conformation
