50e86d2e8f903429e982ab66cc5541ad5c004d99
Patrick Hussey <husspm@gmail.com>
2017-05-11 14:46:37 -0400

    Trying this again

--- db/seeds.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO burgers (burger_name) VALUES ('Bacon Cheeseburger');
-INSERT INTO burgers (burger_name) VALUES ('Greek Burger');
-INSERT INTO burgers (burger_name) VALUES ('Pimento Cheeseburger');
